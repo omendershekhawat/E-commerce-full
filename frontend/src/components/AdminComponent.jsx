cfb39fe8b796932abf75741fc68557ce18d4f033
unknown <omendershekhawat1@gmail.com>
2023-10-10 21:35:46 +0530

    second

--- frontend/src/components/AdminComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-const AdminComponents = () => {
-    const adminAuth = localStorage.getItem("adminData")
-    return adminAuth ? < Outlet /> : <Navigate to="/Adminlogin" />
-}
-
-export default AdminComponents
